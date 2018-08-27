535ca5405ddecf4a81f02000ec4c16d799aac82c
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2018-08-28 00:21:21 +0200

    Make the output of the build scripts more readable

--- svgtopng/pngtheme.sh
+++ svgtopng/pngtheme.sh
@@ -14,26 +14,18 @@ fi
 
 cmd="svgtopng/svgtopng"
 
-echo "==================================="
+echo "== Processing $1"
 
-echo "Creating png icons from svg files and symlinks"
+echo " * Creating png icons from svg files and symlinks"
 #ignore list customized for elementary-xfce
 find "$icondir" -iname "*.svg" -not \( -wholename "*/scalable/*" -o -wholename "*/symbolic/*" -o -wholename "*/animations/*process-*" -o -wholename "*/animations/*gnome-spinner*" -o -wholename "*/animations*pk-action-refresh*" \) -exec $cmd {} +
 
-echo "==================================="
-
-echo "Cleanup icon directory"
+echo " * Cleanup icon directory"
 find "$icondir" -name "untitled folder" -type d -exec rm -rf {} +
 
-echo "==================================="
-
-echo "Deleting svg files"
+echo " * Deleting svg files"
 find "$icondir" -iname '*.svg' -not \( -wholename "*/scalable/*" -o -wholename "*/symbolic/*" -o -wholename "*/animations/*process-*" -o -wholename "*/animations/*gnome-spinner*" -o -wholename "*/animations*pk-action-refresh*" \) -delete
 
-echo "==================================="
-
 #ignore the output if the theme depends on another one (e.g. elementary-xfce-dark needs to be converted before elementary-xfce)
-echo "Checking dangling symlinks"
+echo " * Checking dangling symlinks"
 find -L "$icondir" -type l -exec /bin/ls -go {} \;
-
-echo "==================================="
