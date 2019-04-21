accf5dcc83028f9177324a8968687c0806a1f93c
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2019-04-21 23:24:27 +0200

    Improve symbolic icons for Thunar
    As Thunar wants 24px symbolic icons for its toolbar we provide
    them now instead of setting symbolic icons to a fixed size of
    16px (which is really against their design).

--- svgtopng/pngtheme.sh
+++ svgtopng/pngtheme.sh
@@ -18,13 +18,13 @@ echo "== Processing $1"
 
 echo " * Creating png icons from svg files and symlinks"
 #ignore list customized for elementary-xfce
-find "$icondir" -iname "*.svg" -not \( -wholename "*/scalable/*" -o -wholename "*/symbolic/*" -o -wholename "*/animations/*process-*" -o -wholename "*/animations/*gnome-spinner*" -o -wholename "*/animations*pk-action-refresh*" \) -exec $cmd {} +
+find "$icondir" -iname "*.svg" -not \( -wholename "*/scalable/*" -o -wholename "*/symbolic/*" -o -wholename "*-symbolic.svg" -o -wholename "*/animations/*process-*" -o -wholename "*/animations/*gnome-spinner*" -o -wholename "*/animations*pk-action-refresh*" \) -exec $cmd {} +
 
 echo " * Cleanup icon directory"
 find "$icondir" -name "untitled folder" -type d -exec rm -rf {} +
 
 echo " * Deleting svg files"
-find "$icondir" -iname '*.svg' -not \( -wholename "*/scalable/*" -o -wholename "*/symbolic/*" -o -wholename "*/animations/*process-*" -o -wholename "*/animations/*gnome-spinner*" -o -wholename "*/animations*pk-action-refresh*" \) -delete
+find "$icondir" -iname '*.svg' -not \( -wholename "*/scalable/*" -o -wholename "*/symbolic/*" -o -wholename "*-symbolic.svg" -o -wholename "*/animations/*process-*" -o -wholename "*/animations/*gnome-spinner*" -o -wholename "*/animations*pk-action-refresh*" \) -delete
 
 #ignore the output if the theme depends on another one (e.g. elementary-xfce-dark needs to be converted before elementary-xfce)
 echo " * Checking dangling symlinks"
