c22c705b231c3deef061c3de806850f1cf7ca123
Darin <newhoa@protonmail.com>
2024-04-21 03:07:40 -0400

    Animations/Status: Update process-working icons
    Update process-working and process-working-symbolic icons to match
    the same newer style used for the "network connecting" animations.
    
    In Gtk, the single-frame process-working-symbolic
    is now animated automatically by the GtkSpinner widget.
    
    The mult-frame process-working seems to be still used by Qt,
    so that has been updated (including the additional CSS class
    info that Qt seems to use).
    
    Removed gnome-spinner related animations, PNGs.

--- svgtopng/pngtheme.sh
+++ svgtopng/pngtheme.sh
@@ -18,13 +18,13 @@ echo "== Processing $1"
 
 echo " * Creating png icons from svg files and symlinks"
 #ignore list customized for elementary-xfce
-find "$icondir" -iname "*.svg" -not \( -wholename "*/scalable/*" -o -wholename "*/symbolic/*" -o -wholename "*-symbolic.svg" -o -wholename "*-symbolic-rtl.svg" -o -wholename "*/animations/*process-*" -o -wholename "*/animations/*gnome-spinner*" -o -wholename "*/animations*pk-action-refresh*" \) -exec $cmd {} +
+find "$icondir" -iname "*.svg" -not \( -wholename "*/scalable/*" -o -wholename "*/symbolic/*" -o -wholename "*-symbolic.svg" -o -wholename "*-symbolic-rtl.svg" -o -wholename "*/animations/*process-*" \) -exec $cmd {} +
 
 echo " * Cleanup icon directory"
 find "$icondir" -name "untitled folder" -type d -exec rm -rf {} +
 
 echo " * Deleting svg files"
-find "$icondir" -iname '*.svg' -not \( -wholename "*/scalable/*" -o -wholename "*/symbolic/*" -o -wholename "*-symbolic.svg" -o -wholename "*-symbolic-rtl.svg" -o -wholename "*/animations/*process-*" -o -wholename "*/animations/*gnome-spinner*" -o -wholename "*/animations*pk-action-refresh*" \) -delete
+find "$icondir" -iname '*.svg' -not \( -wholename "*/scalable/*" -o -wholename "*/symbolic/*" -o -wholename "*-symbolic.svg" -o -wholename "*-symbolic-rtl.svg" -o -wholename "*/animations/*process-*" \) -delete
 
 #ignore the output if the theme depends on another one (e.g. elementary-xfce-dark needs to be converted before elementary-xfce)
 echo " * Checking dangling symlinks"
